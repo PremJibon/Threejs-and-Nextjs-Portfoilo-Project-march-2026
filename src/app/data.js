7db4a748a4f9ec8ba7a3f3835833b14cb0aae3a5
Prem Jibon <prempfp96@gmail.com>
2026-03-23 02:23:39 +0600

    Update portfolio code

--- src/app/data.js
+++ src/app/data.js
@@ -81,13 +81,13 @@ export const BtnList = [
   },
   {
     label: "LinkedIn",
-    link: "https://www.linkedin.com/in/codebucks",
+    link: "https://www.linkedin.com/in/shahed-hossain-4891643b9",
     icon: "linkedin",
     newTab: true,
   },
   {
     label: "X",
-    link: "https://www.x.com/code_bucks",
+    link: "https://www.x.com/",
     icon: "twitter",
     newTab: true,
   },
